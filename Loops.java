939b8bd3c671ded73ad29eaabdfc46b71d7bc5f6
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-05-10 01:22:47 +0530

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,21 @@
+//Print all the numbers from 1 to n in separate lines.
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /***Don't change the code here***/
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        inp.close();
+        /***********************************/
+        
+        
+        /***Your code goes here***/
+        for (int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+        
+    }
+}
