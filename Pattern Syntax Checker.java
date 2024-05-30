6e5a5f51494223a73e36f33b8ef9fc11571134f8
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-05-30 23:57:10 +0530

    Create Pattern Syntax Checker.java

---
+++ Pattern Syntax Checker.java
@@ -0,0 +1,32 @@
+//Problem Description
+//The java.util.regex.Pattern class represents a compiled representation of a regular expression.
+//Using Regex, we can easily match or search for patterns in a text. Before searching for a pattern, we have to specify one using some well-defined syntax.
+//In this problem, you are given a pattern. You have to check whether the syntax of the given pattern is valid.
+//Note: In this problem, a regex is only valid if you can compile it using the Pattern.compile method.
+
+import java.lang.*;
+import java.util.*;
+import java.util.Scanner;
+import java.util.regex.*;
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+          	//Write your code
+			
+			try {
+				Pattern.compile(pattern);
+				System.out.println("Valid");
+			}
+			catch(PatternSyntaxException e){
+				System.out.println("Invalid");
+			}
+			testCases--;
+		}
+		in.close();
+        
+    }
+}
